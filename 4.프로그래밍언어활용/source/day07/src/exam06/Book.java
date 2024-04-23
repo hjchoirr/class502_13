316878e585a26b549474d7f0cd1edaaeecf4f9fb
hjchoirr <hjchoir@naver.com>
2024-04-23 17:08:04 +0900

    4/23 저녁

---
+++ 4.프로그래밍언어활용/source/day07/src/exam06/Book.java
@@ -0,0 +1,18 @@
+package exam06;
+
+public class Book extends java.lang.Object{
+    private int isbn;
+    private String title;
+    private String author;
+
+    public Book(int isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("isbn=%d, tite=%s, author=%s%n", this.isbn, this.title, this.author);
+    }
+}
