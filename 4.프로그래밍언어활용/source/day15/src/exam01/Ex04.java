b54707c589de79d8c16b2e8efeed92594d4a8094
hjchoirr <hjchoir@naver.com>
2024-05-07 17:42:33 +0900

    5/7

--- 4.프로그래밍언어활용/source/day15/src/exam01/Ex04.java
+++ 4.프로그래밍언어활용/source/day15/src/exam01/Ex04.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 
 public class Ex04 {
     public static void main(String[] args) {
+        //int[] nums = IntStream.iterate(2, x -> x + 2).limit(10).toArray();
         int[] nums = IntStream.iterate(2, x -> x + 2).limit(10).toArray();
         System.out.println(Arrays.toString(nums));
     }
