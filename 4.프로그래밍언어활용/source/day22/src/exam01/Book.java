568096cb3f3b693b70220e146f806647af773d48
hjchoirr <hjchoir@naver.com>
2024-05-17 16:54:51 +0900

    5/17

--- 4.프로그래밍언어활용/source/day22/src/exam01/Book.java
+++ 4.프로그래밍언어활용/source/day22/src/exam01/Book.java
@@ -3,9 +3,14 @@ package exam01;
 import java.io.Serializable;
 
 public class Book implements Serializable {
+
+    private static final long serialVersionUID = 2L; // 직렬화 호환성 체크용
     private int isbn;
     private String title;
     private transient String author;  // transient :직렬화 배제
+    private int price;
+
+
 
     public Book(int isbn, String title, String author) {
         this.isbn = isbn;
