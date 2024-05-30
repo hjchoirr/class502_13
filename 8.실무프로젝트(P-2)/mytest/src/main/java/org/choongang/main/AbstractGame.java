89ed565915da530de9358690984c223f5a6c4e3a
hjchoirr <hjchoir@naver.com>
2024-05-30 20:13:28 +0900

    5/30

--- 8.실무프로젝트(P-2)/mytest/src/main/java/org/choongang/main/AbstractGame.java
+++ 8.실무프로젝트(P-2)/mytest/src/main/java/org/choongang/main/AbstractGame.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public abstract class AbstractGame implements Game{
 
-
     @Override
     public int getInput() {
         int p = 0;
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+
         if (str.equals("q") || str.equals("quit") || str.equals("exit")) {
             System.exit(0); // 0 : 정상종료, 1: 비정상종료
         }
